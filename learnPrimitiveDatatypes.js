18c2b9caa3cfe30dbafcf17b6ea4180353d71182
sadiyafz <124800419+sadiyafz@users.noreply.github.com>
2026-03-01 14:47:36 +0530

    Add files via upload

---
+++ learnPrimitiveDatatypes.js
@@ -0,0 +1,11 @@
+let firstName = 'Sadiya';
+let companyName = 'TestLeaf';
+let mobileNumber = 987654321
+let knowsAutomation = false
+let usesPlaywright
+
+console.log(typeof(firstName))
+console.log(typeof(companyName))
+console.log(typeof(mobileNumber))
+console.log(typeof(knowsAutomation))
+console.log(typeof(usesPlaywright))
